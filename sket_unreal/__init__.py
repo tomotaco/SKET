1ad24541b0b49597e14fc9c522ab463fbbdd1675
tomotaco <tomotaco@yo.rim.or.jp>
2025-11-23 13:46:35 +0900

    fbx_exportにSKETの更新を適用

--- sket_unreal/__init__.py
+++ sket_unreal/__init__.py
@@ -2,7 +2,7 @@ bl_info = {
     "name": "SKET: SkeletalMesh Export Tool for Unreal Engine",
     "author": "T_Sumisaki",
     "version": (1, 3, 0),
-    "blender": (3, 2, 0),
+    "blender": (5, 0, 0),
     "location": "File > Import-Export",
     "description": "FBX Exporter for Unreal Engine",
     "category": "Import-Export",
